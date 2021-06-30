d99368c8e78fc855d935358f4c91212c746f1ec0
unknown <shreekum9@gmail.com>
2021-06-30 18:23:11 +0530

    git push --set-upstream origin main

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -1,9 +1,10 @@
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalTime;
-import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 
 class RestaurantServiceTest {
